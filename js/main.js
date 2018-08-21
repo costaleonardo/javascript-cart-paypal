84ade7d5f77bac1e8bc889437fcb4af25758fa57
costaleonardo <costaleonardo97@gmail.com>
2018-08-21 09:16:48 -0400

    Add readme.md.

--- js/main.js
+++ js/main.js
@@ -28,7 +28,8 @@ cartBtns.forEach(cartBtn => {
     const item = {
       image: itemDOM.querySelector('.product__image').getAttribute('src'),
       name: itemDOM.querySelector('.product__name').innerText,
-      price: itemDOM.querySelector('.product__price').innerText
+      price: itemDOM.querySelector('.product__price').innerText,
+      quantity: 1
     };
 
     // Check if in cart
@@ -39,8 +40,12 @@ cartBtns.forEach(cartBtn => {
       let itemHTML = `
         <div class="cart__item">
           <img class="cart__item__image" src="${item.image}" alt="${item.name}"/>
-          <h3 class="class__item__name">${item.name}</h3>
-          <h3 class="class__item__price">${item.price}</h3>
+          <h3 class="cart__item__name">${item.name}</h3>
+          <h3 class="cart__item__price">${item.price}</h3>
+          <button class="btn btn--primary btn--small" data-action="DECREASE_ITEM">&minus;</button>
+          <h3 class="cart__item__quantity">${item.quantity}</h3>
+          <button class="btn btn--primary btn--small" data-action="INCREASE_ITEM">&plus;</button>
+          <button class="btn btn--danger btn--small" data-action="REMOVE">&times;</button>
         </div>
       `;
 
@@ -52,6 +57,10 @@ cartBtns.forEach(cartBtn => {
 
       // Push item to cart array
       cartItems.push(item);
+
+      // Get items
+      const items = itemDOM.querySelectorAll('.cart__items');
+
     }
   });
 });
---
+++ readme.md
@@ -0,0 +1,21 @@
+# JavaScript Cart - Paypal Checkout
+
+> Interact with the shopping cart and checkout with Paypal.
+
+## Usage
+
+Clone or download the zip file and open index.html.
+
+## Project Info
+
+### Author
+
+[Leonardo Da Costa](https://github.com/costaleonardo)
+
+### Version
+
+1.0.0
+
+### License
+
+This project is licensed under the MIT License.
