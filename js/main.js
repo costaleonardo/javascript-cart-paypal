7e6af88bff2f0f18f75ed4f404ac65ca3d95b52e
costaleonardo <costaleonardo97@gmail.com>
2018-09-01 15:34:26 -0400

    Add increase functionality.

--- js/main.js
+++ js/main.js
@@ -1,66 +1,49 @@
-'use strict mode'
+'use strict';
 
-/*
- * - ES6 JavaScript Shopping Cart (paypal)
- *
- * - Components:
- *   • DOM Caching
- *
- *
- *
-*/
+let cart = [];
+const cartDOM = document.querySelector('.cart');
+const addToCartButtonsDOM = document.querySelectorAll('[data-action="ADD_TO_CART"]');
 
-// @component DOM Caching
-// @desc      Cache dom elements
-
-const cartItems = [];
-const cart = document.querySelector('.cart');
-const cartBtns = document.querySelectorAll('[data-action="ADD_TO_CART"]');
-
-// Loop throught cart buttons
-cartBtns.forEach(cartBtn => {
-  // Event listener - Click
-  cartBtn.addEventListener('click', () => {
-    // Get parent
-    const itemDOM = cartBtn.parentNode;
-
-    // Create item object
-    const item = {
-      image: itemDOM.querySelector('.product__image').getAttribute('src'),
-      name: itemDOM.querySelector('.product__name').innerText,
-      price: itemDOM.querySelector('.product__price').innerText,
-      quantity: 1
+addToCartButtonsDOM.forEach(addToCartButtonDOM => {
+  addToCartButtonDOM.addEventListener('click', () => {
+    const productDOM = addToCartButtonDOM.parentNode;
+    const product = {
+      image: productDOM.querySelector('.product__image').getAttribute('src'),
+      name: productDOM.querySelector('.product__name').innerText,
+      price: productDOM.querySelector('.product__price').innerText,
+      quantity: 1,
     };
 
-    // Check if in cart
-    const isInCart = (cartItems.filter(cartItem => (cartItem.name === item.name)).length > 0)
+    const isInCart = (cart.filter(cartItem => (cartItem.name === product.name)).length > 0);
 
     if (!isInCart) {
-      // Create new product markup
-      let itemHTML = `
+      cartDOM.insertAdjacentHTML('beforeend', `
         <div class="cart__item">
-          <img class="cart__item__image" src="${item.image}" alt="${item.name}"/>
-          <h3 class="cart__item__name">${item.name}</h3>
-          <h3 class="cart__item__price">${item.price}</h3>
-          <button class="btn btn--primary btn--small" data-action="DECREASE_ITEM">&minus;</button>
-          <h3 class="cart__item__quantity">${item.quantity}</h3>
+          <img class="cart__item__image" src="${product.image}" alt="${product.name}">
+          <h3 class="cart__item__name">${product.name}</h3>
+          <h3 class="cart__item__price">${product.price}</h3>
+          <button class="btn btn--primary btn--small" data-action="DECRESE_ITEM">&minus;</button>
+          <h3 class="cart__item__quantity">${product.quantity}</h3>
           <button class="btn btn--primary btn--small" data-action="INCREASE_ITEM">&plus;</button>
-          <button class="btn btn--danger btn--small" data-action="REMOVE">&times;</button>
+          <button class="btn btn--danger btn--small" data-action="REMOVE_ITEM">&times;</button>
         </div>
-      `;
+      `);
 
-      // Update item status
-      cartBtn.innerText = 'In Cart';
+      cart.push(product);
+      addToCartButtonDOM.innerText = 'In Cart';
 
-      // Insert HTML markup
-      cart.insertAdjacentHTML('beforeend', itemHTML);
-
-      // Push item to cart array
-      cartItems.push(item);
-
-      // Get items
-      const items = itemDOM.querySelectorAll('.cart__items');
-
+      const cartItemsDOM = cartDOM.querySelectorAll('.cart__item');
+      cartItemsDOM.forEach(cartItemDOM => {
+        if (cartItemDOM.querySelector('.cart__item__name').innerText === product.name) {
+          cartItemDOM.querySelector('[data-action="INCREASE_ITEM"]').addEventListener('click', () => {
+            cart.forEach(cartItem => {
+              if (cartItem.name === product.name) {
+                cartItemDOM.querySelector('.cart__item__quantity').innerText = ++cartItem.quantity;
+              }
+            });
+          });
+        }
+      });
     }
   });
 });
